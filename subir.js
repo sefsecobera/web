d24ea480c8da885752adce404e3635b6a3ca2801
sefsecobera <sefsecobera@gmail.com>
2025-09-12 11:06:12 -0300

    Update subir.js

--- subir.js
+++ subir.js
@@ -16,7 +16,7 @@ document.getElementById("formularioImagen").addEventListener("submit", function 
     const base64Data = reader.result.split(',')[1]; // Eliminar encabezado data:image/...
 
     // Reemplazá esta URL con la de tu Web App
-    const URL_WEB_APP = "https://script.google.com/macros/s/AKfycbwexYr8FFoiLoQ7o_TLKNfRpJWOc8PObkr2NWFbXzD_gjuDS6KuxKQqtPzijDVY30HS/exec";
+    const URL_WEB_APP = "https://script.google.com/macros/s/AKfycbxCPmcOpYiwI7xYfx2cJw72kZbuFsF37xTVZ6Gy8bBwZjBT7ChaQcohhxg_IBiHUGII/exec";
 
     const formData = new URLSearchParams();
     formData.append("imagen", base64Data);
@@ -59,6 +59,7 @@ document.getElementById("formularioImagen").addEventListener("submit", function 
 
   reader.readAsDataURL(imagen);
 });
+
 
 
 
